bf3c5ee052230bc8fbc20e25b0b778e502c86883
lnpatrone-spec <lnpatrone@gmail.com>
2026-09-18 22:35:19 +0200

    Solución al ejercicio 1.a terminada

--- ejercicios/malla.c
+++ ejercicios/malla.c
@@ -1,21 +1,48 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-main (int argc, char *argv[]) {
-    int pid = 1; 
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Argumentos incorrectos\n");
+    } else {
+        int filas = atoi(argv[1]);
+        int columnas = atoi(argv[2]);
+        int pid = 1;
+        int soy_raiz = 1;
+        pid_t pid_raiz = getpid();
 
-    int filas = atoi(argv[1]);
-    int columnas = atoi(argv[2]);
-
-    for (int i = 0; i < (columnas) && pid > 0; i++) {
-        pid = fork();
-    }
-
-    if (pid == 0) {
-        for (int j = 0; j < (filas - 1) && pid == 0; j++) {
+        for (int i = 0; i < columnas && pid > 0; i++) {
             pid = fork();
+            if (pid == 0) {
+                soy_raiz = 0;
+            }
+        }
+
+        if (!soy_raiz) {
+            for (int j = 0; j < filas - 1 && pid == 0; j++) {
+                pid = fork();
+            }
+        }
+
+        if (soy_raiz) {
+            sleep(1);
+
+            char orden[32];
+            sprintf(orden, "pstree -c %d", pid_raiz);
+            system(orden);
+
+            for (int i = 0; i < columnas; i++) {
+                wait(NULL);
+            }
+        } else {
+            if (pid > 0) {
+                wait(NULL);
+            } else {
+                sleep(3); // sino no salen
+            }
         }
     }
     return 0;
-} 
+}
