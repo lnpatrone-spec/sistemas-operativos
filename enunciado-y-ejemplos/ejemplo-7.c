554024ea8e048f01de0773308c78177358ae98cb
lnpatrone-spec <lnpatrone@gmail.com>
2026-09-20 18:21:11 +0200

    Comentario del ejemplo 7
    Added a comment explaining the program's functionality.

--- enunciado-y-ejemplos/ejemplo-7.c
+++ enunciado-y-ejemplos/ejemplo-7.c
@@ -1,3 +1,8 @@
+/* El programa demuestra cómo un proceso puede provocar su propia finalización inmediata enviándose
+una orden de detención del sistema. Tras mostrar un aviso previo, el proceso consulta su propia identidad
+y se dirige a sí mismo una instrucción no interceptable ni cancelable, lo que ocasiona su cierre en ese
+preciso instante e impide por completo alcanzar las líneas posteriores. */
+
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
