44333c0bfe85a2bc1d664eaabd7056b08d59e04b
lnpatrone-spec <lnpatrone@gmail.com>
2026-09-20 18:12:26 +0200

    Comentario del ejemplo 4 PROG 2
    Added comments explaining the program's functionality and purpose.

--- enunciado-y-ejemplos/ejemplo-4-prog2.c
+++ enunciado-y-ejemplos/ejemplo-4-prog2.c
@@ -1,3 +1,9 @@
+/* Este programa actúa como el ejecutable de destino. Entra en funcionamiento asumiendo el control
+directo de un proceso preexistente que acaba de sustituir su contenido original. Su función consiste en
+recibir los parámetros que le fueron transferidos desde el programa invocador, imprimirlos por pantalla
+para validar la persistencia de los datos entre ambas etapas, sostener una breve espera y culminar
+formalmente la ejecución del proceso en el sistema operativo. */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,7 +14,7 @@ int main (int argc, char *argv[]) {
  
     for ( i = 0; i < argc; i ++ ) {
         printf (" argv[%d] : %s \n", i, argv[i]);
+    }
         sleep(10);
         exit (0);
-    }
 } 
