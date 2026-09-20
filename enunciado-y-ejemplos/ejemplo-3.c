8dea2e42020a1edd0d17fd7e4ee2744aa568f260
lnpatrone-spec <lnpatrone@gmail.com>
2026-09-20 18:06:00 +0200

    Comentario del ejemplo 3

--- enunciado-y-ejemplos/ejemplo-3.c
+++ enunciado-y-ejemplos/ejemplo-3.c
@@ -1,3 +1,11 @@
+/* El programa coordina la sincronización entre dos procesos para evaluar el motivo exacto de la
+finalización de un proceso hijo. Tras la creación del proceso secundario, el proceso padre detiene
+temporalmente su avance hasta que el hijo termina. El proceso hijo finaliza retornando un código
+de estado númerico (salvo que sea interrumpido externamente antes de tiempo). Al reanudar, el
+proceso padre analiza la información de cierre recivida para distinguir e informar si el proceso
+hijo finalizó de manera voluntaria reportando su valor de salida, o si fue forzado a terminar a 
+causa de una señal del sistema operativo. */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
