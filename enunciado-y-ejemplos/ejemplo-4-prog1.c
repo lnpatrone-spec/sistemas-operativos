ac0d7cc05ac5b70ea7f8948e8c48dcd5eeba1e38
lnpatrone-spec <lnpatrone@gmail.com>
2026-09-20 18:10:27 +0200

    Comentario del ejemplo 4 PROG 1
    Add comments explaining the program's functionality and behavior.

--- enunciado-y-ejemplos/ejemplo-4-prog1.c
+++ enunciado-y-ejemplos/ejemplo-4-prog1.c
@@ -1,3 +1,9 @@
+/* Este programa actúa como invocador inicial. Al arrancar, recibe un conjunto de parámetros desde la
+línea de comandos, los muestra por pantalla y, tras una breve pausa temporal, sustituye de forma
+completa su propia lógica de ejecución por la de un segundo programa ejecutable, transfiriéndole los
+argumentos recibidos. Mediante esto, el programa original delega el control al nuevo programa sin
+necesidad de crear un proceso adicional, conservando la misma entidad operativa ante el sistema. */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -9,9 +15,10 @@ int main (int argc, char *argv[]) {
     
         for ( i = 0; i < argc; i ++ ) {
         printf (" argv[%d] : %s \n", i, argv[i]);
+        }
         sleep( 10 );
         strcpy (argv[0],"prog2");
-        }
+        
 
         if (execvp ("./prog2", argv) < 0) {
         printf ("Error en la invocacion a prog2 \n");
